4c96c768c001b79aea5bed188e2c466f46595a97
MurilloNS <murillo.murillo2001@gmail.com>
2022-12-05 10:49:23 -0300

    Making Navbar Responsible

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import {
+  Nav,
+  NavMenu,
+  NavLink,
+  LogoImg,
+  NavToggler,
+  NavTogglerClose,
+} from "./styles";
+
+export default function Navbar() {
+  const [active, setActive] = useState(false);
+  const showActive = () => setActive(!active);
+
+  return (
+    <>
+      <Nav>
+        <LogoImg src={require("../../assets/warner.png")} alt="logo" />
+        <NavMenu show={active}>
+          <NavLink to="/films">Filmes</NavLink>
+          <NavLink to="/series">Séries</NavLink>
+          <NavLink to="/people">Pessoas</NavLink>
+        </NavMenu>
+        {!active ? (
+          <NavToggler onClick={showActive} />
+        ) : (
+          <NavTogglerClose onClick={showActive} />
+        )}
+      </Nav>
+    </>
+  );
+}
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { NavLink as Link } from "react-router-dom";
+import { BsList, BsXLg } from "react-icons/bs";
 
 export const Nav = styled.nav`
   background-color: #1c2833;
@@ -9,11 +10,15 @@ export const Nav = styled.nav`
   align-items: center;
   justify-content: space-evenly;
   padding: 0 20em 0 20em;
+
+  @media screen and (max-width: 768px) {
+    padding: 0 20em 0 0;
+  }
 `;
 
 export const LogoImg = styled.img`
-    height: 4em;
-    width: 4em;
+  height: 4em;
+  width: 4em;
 `;
 
 export const NavMenu = styled.div`
@@ -22,15 +27,66 @@ export const NavMenu = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+
+  @media screen and (max-width: 768px) {
+    position: fixed;
+    top: 7vh;
+    right: 0;
+    width: 50%;
+    height: 93vh;
+    background-color: #1c2833;
+    flex-direction: column;
+    transform: ${(props) =>
+      props.show ? "translateX(0)" : "translateX(100%)"};
+    transition: 0.3s;
+  }
 `;
 
 export const NavLink = styled(Link)`
-  color:   #616a6b;
+  color: #616a6b;
   margin: 0 2em 0 0;
   font-size: 1.1em;
   font-weight: bold;
 
-  &.active{
+  &.active {
     color: #ecf0f1;
+  }
+
+  @media screen and (max-width: 768px) {
+    margin-top: 2vh;
+  }
+`;
+
+export const NavToggler = styled(BsList)`
+  color: #fff;
+  height: 2rem;
+  width: 2rem;
+  cursor: pointer;
+  display: none;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 75%);
+    cursor: pointer;
+  }
+`;
+
+export const NavTogglerClose = styled(BsXLg)`
+  color: #fff;
+  height: 1.4rem;
+  width: 1.4rem;
+  cursor: pointer;
+  display: none;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-170%, 125%);
+    cursor: pointer;
   }
 `;
